423045648a6692a8db0b47666e8f879ac5f95127
maceip <ryan.macarthur@gmail.com>
2024-03-14 00:09:00 -0400

    split out webapps, add local certs for ssl

--- packages/webapp-prove/src/App.tsx
+++ packages/webapp-prove/src/App.tsx
@@ -1,14 +1,54 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react';
+import { prove, verify } from 'tlsn-js';
+import { Proof } from 'tlsn-js/build/types';
+import tlsnLogo from './assets/tlsn.svg'
+
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [processing, setProcessing] = useState(false);
+  const [result, setResult] = useState<{
+    time: number;
+    sent: string;
+    recv: string;
+    notaryUrl: string;
+  } | null>(null);
+  const [proof, setProof] = useState<Proof | null>(null);
+
+  const onClick = useCallback(async () => {
+    setProcessing(true);
+    const p = await prove('https://example.com', {
+      method: 'GET',
+      body: '',
+      headers: {
+      'Accept-Encoding': 'identity',
+      'Connection': 'close',
+      },
+      maxTranscriptSize: 4096,
+      notaryUrl: 'https://local.proof.markets:7047',
+      websocketProxyUrl: 'wss://local.proof.markets:55688',
+    });
+    setProof(p);
+  }, [setProof, setProcessing]);
+
+  useEffect(() => {
+    (async () => {
+      if (proof) {
+        const r = await verify(proof);
+        setResult(r);
+        setProcessing(false);
+      }
+    })();
+  }, [proof, setResult]);
 
   return (
     <>
       <div>
+      <a href="https:/" target="_blank">
+          <img src={tlsnLogo} className="logo" alt="TLSN logo" />
+        </a>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,18 +56,44 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>tlsn: create a proof demo</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+      <button onClick={!processing ? onClick : undefined} disabled={processing}>
+        Click to create a Proof!
+      </button>
+      <div>
+        <b>Proof: </b>
+        {!processing && !proof ? (
+          <i>not started</i>
+        ) : !proof ? (
+          <>
+            Proving data from example.com...
+
+            Open <i>Developer tools</i> to follow progress
+          </>
+        ) : (
+          <>
+            <details>
+              <summary>View Proof</summary>
+              <pre>{JSON.stringify(proof, null, 2)}</pre>
+            </details>
+          </>
+        )}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <b>Verification: </b>
+        {!proof ? (
+          <i>not started</i>
+        ) : !result ? (
+          <i>verifying</i>
+        ) : (
+          <pre>{JSON.stringify(result, null, 2)}</pre>
+        )}
+      </div>
+    </div>
+      </div>
+   
     </>
   )
 }
--- packages/webapp-prove/vite.config.ts
+++ packages/webapp-prove/vite.config.ts
@@ -1,75 +1,57 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
-import inject from '@rollup/plugin-inject';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { nodePolyfills } from "vite-plugin-node-polyfills"
+import inject from "@rollup/plugin-inject"
+import { viteStaticCopy } from "vite-plugin-static-copy"
 
 export default defineConfig({
-  //assetsInclude: ["**/tlsn-js/build/*.wasm"],
-  plugins: [react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'node_modules/tlsn-js/build/*.wasm',
-          dest: 'packages/dapp/public'
-        }
-      ]
-    }),
-            nodePolyfills({
-      include: ["buffer"],
-      globals: {
-        Buffer: true,
-      },
-    }),
-           
-           ],
-           preview: {
-            proxy: {
-              "/*": {
-                target: 'http://127.0.0.1:4173/',
-                changeOrigin: true,
-                configure: (proxy, options) => {
-                  options.headers = {
-                    "Cross-Origin-Embedder-Policy": "require-corp",
-                    'Cross-Origin-Opener-Policy': 'same-origin',
-                  }
-                },
-              },
-            },
-            headers: {
-              'Cross-Origin-Embedder-Policy': 'require-corp',
-              'Cross-Origin-Opener-Policy': 'same-origin',
-            },
-           },
-  server: {
-    https: { key: "../authdecode/notary-server/fixture/tls/notary.key",
-            cert: "../authdecode/notary-server/fixture/tls/notary.crt"
-  },
-    host: '127.0.0.1',
-    port: 3000,
-    cors: false,
-    headers: {
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Opener-Policy': 'same-origin',
-    },
-    fs: {
-      strict: false,
-    },
-  },
-  build: {
-    target: "esnext",
-    outDir: "build",
-    minify: true,
-    sourcemap: true,
-    rollupOptions: {
-      plugins: [
-        inject({
-          // cbor-x checks for Buffer on the global object, and the polyfills plugin doesn't cover this case for the
-          // production build (but works in development because Buffer gets injected as a banner, so it's "naturally"
-          // available on the global object)
-          "globalThis.Buffer": ["buffer", "Buffer"],
-        }),
-      ],
-    },
-  },
-});
+	//assetsInclude: ["**/tlsn-js/build/*.wasm"],
+	publicDir: "dist",
+	plugins: [
+		react(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: "node_modules/tlsn-js/build/*",
+					dest: ".",
+				},
+			],
+		}),
+		nodePolyfills({
+			include: ["buffer"],
+			globals: {
+				Buffer: true,
+			},
+		}),
+	],
+	server: {
+		proxy: {
+			"/*": {
+				target: "https://127.0.0.1:4443/",
+				changeOrigin: true,
+				configure: (proxy, options) => {
+					options.headers = {
+						"Cross-Origin-Embedder-Policy": "require-corp",
+						"Cross-Origin-Opener-Policy": "same-origin",
+					}
+				},
+			},
+		},
+		https: { key: "../common/fixture/tls/privkey1.pem", cert: "../common/fixture/tls/cert1.pem" },
+		host: "local.proof.markets",
+		port: 4443,
+		cors: false,
+		headers: {
+			"Cross-Origin-Embedder-Policy": "require-corp",
+			"Cross-Origin-Opener-Policy": "same-origin",
+		},
+		fs: {
+			strict: false,
+		},
+	},
+	build: {
+		target: "esnext",
+		minify: true,
+		sourcemap: true,
+	},
+})
